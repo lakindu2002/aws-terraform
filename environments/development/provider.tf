e568a0b48ba914a17dae309328d676091781c6bc
lakindu hewawasam <lakinduhewa@gmail.com>
2025-05-25 17:06:11 +0530

    feat: updating bucket name

--- environments/development/provider.tf
+++ environments/development/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "lakindus-terraform-state-storage"
+    bucket = "lakindus-terraform-state"
     key    = "development/terraform.tfstate"
     region = "us-east-1"
   }
